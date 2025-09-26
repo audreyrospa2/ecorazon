bb14e1ded1e882ff21070d1b84023d1fcd249e85
audreyrospa <audreyrospa@gmai.com>
2025-09-26 16:58:52 +0700

    Update configs and compress images for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  integrations: [tailwind()],
+  site: 'https://github.com/audreyrospa/ecorazon-site.git',
+  base: '/<ecorazon-site>/',
 });
