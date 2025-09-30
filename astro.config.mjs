2a5c170dfe6eddd270da65a85b39933675182474
audreyrospa <audreyrospa@gmai.com>
2025-09-30 16:02:09 +0700

    Add Tailwind CSS import

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,12 @@
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 
+// https://docs.astro.build/en/reference/configuration-reference/
 export default defineConfig({
-  site: 'https://github.com/audreyrospa/ecorazon-site.git',
-  base: '/<ecorazon-site>/',
+  integrations: [tailwind()],
+  output: "static", // default for static sites
+  server: {
+    port: 4321, // or change to any port you like
+    host: true
+  }
 });
